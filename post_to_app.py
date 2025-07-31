6e6244b884976920abcb3929548c7405faf06065
StadynR <stadynr@gmail.com>
2025-07-31 15:38:20 -0500

    Implementación de request a la app usando argumentos de consola

--- post_to_app.py
+++ post_to_app.py
@@ -1,14 +1,20 @@
+import argparse
 import requests
+
+parser = argparse.ArgumentParser(description="Send a query to the product RAG bot")
+parser.add_argument("-q", "--query", required=True, type=str, help="The question to send")
+args = parser.parse_args()
+
 
 url = "http://localhost:8000/query"
 payload = {
-    "user_id": "usuario_demo",
-    "query": "¿Qué juegos están disponibles en tienda?"
+    "user_id": "user_demo",
+    "query": args.query
 }
 
 response = requests.post(url, json=payload)
 if response.ok:
-    print("Respuesta de la app:")
+    print("App's response:")
     print(response.json())
 else:
     print(f"Error {response.status_code}: {response.text}")
